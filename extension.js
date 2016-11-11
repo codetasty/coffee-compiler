cb1704e3ee2e8d9b7da0bbe6730b94fd9ac54905
Andrej Adamcik <ani.mack@gmail.com>
2016-11-11 21:52:59 +0000

    api updates

--- extension.js
+++ extension.js
@@ -18,11 +18,15 @@ define(function(require, exports, module) {
 		init: function() {
 			var self = this;
 			
-			EditorSession.on('save', function(e) {
-				if (self._exts.indexOf(e.storage.extension) !== -1) {
-					Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.getValue());
-				}
-			});
+			EditorSession.on('save', this.onSave);
+		},
+		destroy: function() {
+			EditorSession.off('save', this.onSave);
+		},
+		onSave: function(e) {
+			if (Extension._exts.indexOf(e.storage.extension) !== -1) {
+				Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.getValue());
+			}
 		},
 		_exts: ['coffee'],
 		importWorkspace: null,
@@ -63,5 +67,5 @@ define(function(require, exports, module) {
 		}
 	});
 
-	module.exports = Extension;
+	module.exports = Extension.api();
 });
